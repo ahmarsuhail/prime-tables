d4ee1d1ecf4d684d5d9d6548fbf5e9941f468b6a
Ahmar Suhail <ahmar.suhail@gmail.com>
2019-07-30 15:56:31 -0700

    Changes for generating table

--- primeTable.js
+++ primeTable.js
@@ -2,7 +2,7 @@
 const generatePrimes = (numberOfPrimes) => {
 
     const result = [];
-    const findPrimesTill = Math.round(numberOfPrimes * Math.log2(numberOfPrimes));
+    const findPrimesTill = (numberOfPrimes < 4) ? 6: Math.round(numberOfPrimes * Math.log2(numberOfPrimes));
     const isPrime = new Array(findPrimesTill).fill(true);
     
     for(let i=2; i<Math.sqrt(findPrimesTill); i++) {
@@ -27,7 +27,31 @@ const generatePrimes = (numberOfPrimes) => {
     return result;
 }
 
+
+const generateTable = (numberOfPrimes) => {
+    const primes = generatePrimes(numberOfPrimes);
+    const table = [];
+    
+    for(let i=0; i<primes.length; i++) {
+        let temp = [];
+        temp.push(primes[i]);
+        table.push(temp);
+    }
+
+    primes.unshift(' ');
+    table.unshift(primes);
+
+    for(let i=1; i<table.length; i++) {
+        for(let j=1; j<primes.length; j++) {
+            table[i].push(table[i][0]*primes[j]);
+        }
+    }
+
+    return table;
+}
+
 module.exports = {
-    generatePrimes
+    generatePrimes,
+    generateTable
 }
 
--- test/primeTableTest.js
+++ test/primeTableTest.js
@@ -1,31 +1,56 @@
-const {generatePrimes} = require('../primeTable')
+const { generatePrimes, generateTable } = require('../primeTable')
 const primeNumberLists = require('./primeNumberLists.json');
+const primeNumberTables = require('./primeNumberTables');
 const expect = require('chai').expect;
 const assert = require('assert');
 
-describe('Prime table generator', function() {
-  
-  describe('generatePrimes()', function() {
-    
-    it('should generate first five prime numbers', function() {
+describe('Prime table generator', function () {
+
+  describe('generatePrimes()', function () {
+
+    it('should generate first three prime numbers', function () {
+      expect(primeNumberLists.firstThreePrimes).to.eql(generatePrimes(3));
+    });
+
+    it('should generate first five prime numbers', function () {
       expect(primeNumberLists.firstFivePrimes).to.eql(generatePrimes(5));
     });
 
-    it('should generate first hundred prime numbers', function() {
+    it('should generate first hundred prime numbers', function () {
       expect(primeNumberLists.firstHundredPrimes).to.eql(generatePrimes(100));
     });
-    
 
-    it('should generate first ten thousand prime numbers', function() {
+
+    it('should generate first ten thousand prime numbers', function () {
       expect(primeNumberLists.firstTenThousandPrimes).to.eql(generatePrimes(10000));
     });
 
-    it('should generate first thousand prime numbers', function() {
+    it('should generate first thousand prime numbers', function () {
       const twentyThousandPrimes = generatePrimes(20000);
       assert.equal(twentyThousandPrimes.length, 20000);
       assert.equal(twentyThousandPrimes[twentyThousandPrimes.length - 1], 224737);
     });
 
   });
+
+  describe('generateTable()', function () {
+
+    it('should generate table for first three prime numbers', function () {
+      threePrimesTable = generateTable(3);
+
+      for (let i = 0; i <= 3; i++) {
+        expect(primeNumberTables.threePrimesTable[i]).to.eql(threePrimesTable[i]);
+      }
+    })
+
+    it('should generate table for first five prime numbers', function () {
+      fivePrimesTable = generateTable(5);
+
+      for (let i = 0; i <= 3; i++) {
+        expect(primeNumberTables.fivePrimesTable[i]).to.eql(fivePrimesTable[i]);
+      }
+    })
+
+  })
 
 });
